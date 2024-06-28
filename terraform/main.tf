674f081bdc672ccb95e5330151626ed2c5b356ba
unknown <shivammahajan221@gmail.com>
2024-06-28 18:25:52 +0530

    new

---
+++ terraform/main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region = var.region
+}
+
+module "autoscaling" {
+  source           = "/root/devops/terraform/modules/autoscalling"
+  name             = var.name
+  region           = var.region
+  ami              = var.ami
+  instance_type    = var.instance_type
+  desired_capacity = var.desired_capacity
+  max_size         = var.max_size
+  min_size         = var.min_size
+  subnet_ids       = var.subnet_ids
+}
