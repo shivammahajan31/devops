c5052b18da4a0b6f7619fbad377219635996bed7
unknown <shivammahajan221@gmail.com>
2024-06-29 01:02:48 +0530

    f

--- terraform/vpc_mod_use/main.tf
+++ terraform/vpc_mod_use/main.tf
@@ -4,9 +4,9 @@ provider "aws" {
 
 module "vpc" {
   source               = "/root/devops/terraform/modules/vpc"
-  vpc_cidr             = var.vpc_cidr_block
+  vpc_cidr_block       = var.vpc_cidr_block
   vpc_name             = var.vpc_name
-  public_subnet_cidr  = var.public_subnet_cidr
+  public_subnet_cidr   = var.public_subnet_cidr
   private_subnet_cidr  = var.private_subnet_cidr
   availability_zones   = var.availability_zones
 }
