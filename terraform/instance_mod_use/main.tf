435feb47d1c42464260ba42ac80ab8d05ad9c581
unknown <shivammahajan221@gmail.com>
2024-06-29 00:15:57 +0530

    d

---
+++ terraform/instance_mod_use/main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  region = var.region
+}
+
+resource "module" {
+    source = "/root/devops/terraform/modules/instance"
+    instance_name = var.instance_name
+    ami_id = var.ami_id
+    instance_type = var.instance_type
+    key_name = var.key_name
+    vpc_id = var.vpc_id
+    vpc_subnet_id = var.vpc_subnet_id
+    vpc_security_group_ids = var.vpc_security_group_ids
+
+
+}
--- terraform/instance_mod_use/variable.tf
+++ terraform/instance_mod_use/variable.tf
@@ -1,24 +1,33 @@
 variable "region" {
-    type = string
+    type = string 
+    default = "us-east-1"
 }
 variable "instance_name" {
     type = string
+    default = "new_instance"
 }
 variable "ami_id" {
     type = string
+    default = "ami-0e001c9271cf7f3b9"
 }
 variable  "instance_type" {
     type = string
+    default = "t2.micro"
 }
 variable "key_name" {
-    type = string
+    type = string 
+    default = "kb.pem"
 }
 variable "vpc_id" {
     type = string
+    default = "vpc-052cce6d50c79deac"
 }
 variable "vpc_subnet_id" {
     type = string
+    default = "subnet-0847f124a7bf05660"
 }
 variable "vpc_security_group_ids" {
     type = list(string)
+    default = ["sg-03e1e5c3310d57ff7"]
+
 }
