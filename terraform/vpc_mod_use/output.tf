af35ff23602c6822a00e5b0b2cd4004dc2ee05b9
unknown <shivammahajan221@gmail.com>
2024-06-29 01:55:31 +0530

    D

--- terraform/vpc_mod_use/output.tf
+++ terraform/vpc_mod_use/output.tf
@@ -1,15 +1,15 @@
 output "myvpc_id" {
-    value = module.aws_vpc.myvpc_id
+    value = module.myvpc_id
 }
 output "private_subnet_id" {
-    value = module.aws_subnet.private_subnet_id
+    value = module.private_subnet_id
 }
 output "public_subnet_id" {
-    value = module.aws_subnet.public_subnet_id
+    value = module.public_subnet_id
 }
 output "internet_gateway_id" {
-    value = module.aws_internet_gateway.internet_gateway_id
+    value = module.internet_gateway_id
 }
 output "public_route_table_id" {
-    value = module.aws_route_table.public_route_table_id
+    value = module.public_route_table_id
 }
