8b2a353f6ec2a25732c9544aad257bebc47ebe3f
unknown <shivammahajan221@gmail.com>
2024-06-28 19:40:40 +0530

    s

--- terraform/output.tf
+++ terraform/output.tf
@@ -2,7 +2,7 @@ output "autoscaling_group_name" {
   description = "Name of the auto-scaling group"
   value       = module.autoscaling.autoscaling_group_name
 }
-output "autoscaling_group_name" {
+output "aws_launch_configuration_id" {
   description = "Name of the auto-scaling group"
   value       = module.autoscaling.aws_launch_configuration_id
 }
