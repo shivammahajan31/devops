5261a8549b82fb9514242959c4ae1b5b677a5772
unknown <shivammahajan221@gmail.com>
2024-06-28 12:54:08 +0530

    new files

---
+++ terraform/modules/instance/main.tf
@@ -0,0 +1,15 @@
+resource "aws_instance" "demo_instance" {
+  ami = var.ami_id
+  instance_type = var.instance_type
+  key_name   = var.key_name
+  vpc_security_group_ids =  "${var.vpc_security_group_ids}"
+  vpc_id = var.vpc_id
+  vpc_subnet_id = var.vpc_subnet_id
+
+
+
+
+  tags = {
+    Name = "${var.instance_name}"-demo_instance
+  }     
+}
---
+++ terraform/modules/instance/variable.tf
@@ -0,0 +1,7 @@
+variable "instance_name" {}
+variable "ami_id" {}
+variable  "instance_type" {}
+variable "key_name" {}
+variable "vpc_id" {}
+variable "vpc_subnet_id" {}
+variable "vpc_security_group_ids" {}
